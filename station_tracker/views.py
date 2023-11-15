bd24a1254a79b781c69c0c5d3dddf2e4f6d0bc35
Gouthgithub <144191315+Gouthgithub@users.noreply.github.com>
2023-11-15 21:50:35 +0000

    updated the gas station crud functionality

--- station_tracker/views.py
+++ station_tracker/views.py
@@ -1,28 +1,42 @@
-from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
-from django.contrib.auth import authenticate, login, logout 
 
-from .forms import UserCreationForm, LoginForm, GasPriceUpdateForm, FeedbackForm
-from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
+
 from .models import Feedback, AboutUs, Gas_Station
-from django.http import HttpResponse
 from geopy.geocoders import Nominatim
 from geopy.distance import Geodesic
 import folium
+<<<<<<< HEAD
 from .forms import UserCreationForm, LogiForm, GasStationOwnerForm, GasStationListingForm, GasStationReviewForm, CustomerInquiryForm
+=======
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import (
+    UserCreationForm,
+    LoginForm,
+    GasStationOwnerForm,
+    GasStationForm,
+    GasStationListingForm,
+    GasStationReviewForm,
+    CustomerInquiryForm,
+    GasPriceUpdateForm, 
+    FeedbackForm
+)
+
+>>>>>>> cfe58171 (updated the gas station crud functionality)
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
-from .models import GasStationOwner, GasStationListing, GasStationReview, CustomerInquiry,GasStation
+from .models import GasStationOwner, GasStationListing, GasStationReview, CustomerInquiry, GasStation
 from django.core.exceptions import PermissionDenied
 from django.views.generic import ListView
-
 
 # Create your views here.
 #def home(request):
 # return render(request, 'index.html')
+
 
 # Create your views here.
 # Home page
@@ -38,6 +52,7 @@ def main(request):
   context = {'user': user}
   return render(request, 'main.html', context)
 
+
 # signup page
 def user_signup(request):
     if request.method == 'POST':
@@ -49,6 +64,7 @@ def user_signup(request):
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
 
+
 # login page
 def user_login(request):
     if request.method == 'POST':
@@ -59,8 +75,10 @@ def user_login(request):
             user = authenticate(request, username=username, password=password)
             if user:
                 login(request, user)
+
                # form.save()
                 messages.info(request, f"You are now logged in as {username}.")
+
                 return redirect('home')
             else:
                 messages.error(request, "Invalid username or password.")
@@ -68,6 +86,7 @@ def user_login(request):
         form = LoginForm()
    # return render(request, 'login.html', {'form': form}, {'user': request.user})
         return render(request, 'login.html', {'user': request.user, 'form': form})
+
 
 # logout page
 def user_logout(request):
@@ -136,14 +155,15 @@ def user_payment(request):
   return render(request, 'payment.html')
 
 
-
 # Authentication decorator
 def user_authenticated(view_func):
     def wrapper(request, *args, **kwargs):
         if not request.user.is_authenticated:
             return HttpResponse("Authentication Required", status=401)
         return view_func(request, *args, **kwargs)
+
     return wrapper
+
 
 @csrf_exempt
 @login_required
@@ -153,13 +173,12 @@ def create_gas_station_owner(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Gas station owner created successfully')
-            return redirect('home')  
+            return redirect('home')
         else:
             messages.error(request, 'Error creating gas station owner')
     else:
         form = GasStationOwnerForm()
     return render(request, 'owner_form.html', {'form': form})
-
 
 
 @csrf_exempt
@@ -169,13 +188,15 @@ def create_gas_station_listing(request):
         form = GasStationListingForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Gas station listing created successfully')
+            messages.success(request,
+                             'Gas station listing created successfully')
             return redirect('gas_station_list')  # Redirect to a success page
         else:
             messages.error(request, 'Error creating gas station listing')
     else:
         form = GasStationListingForm()
     return render(request, 'listing_form.html', {'form': form})
+
 
 @csrf_exempt
 @login_required
@@ -185,13 +206,15 @@ def update_gas_station_listing(request, listing_id):
         form = GasStationListingForm(request.POST, instance=listing)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Gas station listing updated successfully')
+            messages.success(request,
+                             'Gas station listing updated successfully')
             return redirect('gas_station_list')  # Redirect to a success page
         else:
             messages.error(request, 'Error updating gas station listing')
     else:
         form = GasStationListingForm(instance=listing)
     return render(request, 'listing_form.html', {'form': form})
+
 
 @csrf_exempt
 @login_required
@@ -212,13 +235,16 @@ def create_gas_station_review(request):
         form = GasStationReviewForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Gas station review created successfully')
+            messages.success(request,
+                             'Gas station review created successfully')
             return redirect('/')  # Redirect to a success page
         else:
             messages.error(request, 'Error creating gas station review')
     else:
         form = GasStationReviewForm()
     return render(request, 'review_form.html', {'form': form})
+
+
 # Update gas station review
 @csrf_exempt
 @login_required
@@ -228,13 +254,15 @@ def update_gas_station_review(request, review_id):
         form = GasStationReviewForm(request.POST, instance=review)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Gas station review updated successfully')
+            messages.success(request,
+                             'Gas station review updated successfully')
             return redirect('/')  # Redirect to a success page
         else:
             messages.error(request, 'Error updating gas station review')
     else:
         form = GasStationReviewForm(instance=review)
     return render(request, 'review_form.html', {'form': form})
+
 
 # Delete gas station review
 @csrf_exempt
@@ -247,7 +275,9 @@ def delete_gas_station_review(request, review_id):
         return redirect('/')  # Redirect to a success page
     return render(request, 'confirm_delete_review.html', {'review': review})
 
+
 # Create customer inquiry
+
 
 @csrf_exempt
 @login_required
@@ -264,6 +294,7 @@ def update_gas_station_owner(request, owner_id):
     else:
         form = GasStationOwnerForm(instance=owner)
     return render(request, 'owner_form.html', {'form': form})
+
 
 @csrf_exempt
 @login_required
@@ -291,6 +322,7 @@ def create_customer_inquiry(request):
         form = CustomerInquiryForm()
     return render(request, 'inquiry_form.html', {'form': form})
 
+
 @csrf_exempt
 @login_required
 def update_customer_inquiry(request, inquiry_id):
@@ -307,6 +339,7 @@ def update_customer_inquiry(request, inquiry_id):
         form = CustomerInquiryForm(instance=inquiry)
     return render(request, 'inquiry_form.html', {'form': form})
 
+
 @csrf_exempt
 @login_required
 def delete_customer_inquiry(request, inquiry_id):
@@ -317,11 +350,71 @@ def delete_customer_inquiry(request, inquiry_id):
         return redirect('/')  # Redirect to a success page
     return render(request, 'confirm_delete.html', {'inquiry': inquiry})
 
+
 # def gas_station_list(request):
 #     gas_stations = GasStationListing.objects.all()
 #     return render(request, 'gas_station_list.html', {'gas_stations': gas_stations})
 class GasStationListView(ListView):
+<<<<<<< HEAD
   model = GasStationListing
   template_name = 'gas_station_list.html'
   context_object_name = 'gas_stations'
 >>>>>>> 173a3f15 (created the owner , gas station, reviews ,and  inquiry  functionality thst spplies crud)
+=======
+    model = GasStationListing
+    template_name = 'gas_station_list.html'
+    context_object_name = 'gas_stations'
+
+
+def gas_station_form(request, action, gas_station=None):
+    form = GasStationForm(
+        instance=gas_station) if gas_station else GasStationForm()
+
+    context = {
+        'form': form,
+        'action': action,
+        'gas_station': gas_station,
+    }
+
+    return render(request, 'gas_station_form.html', context)
+
+
+def create_gas_stations(request):
+    if request.method == 'POST':
+        form = GasStationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect(
+                'ga-stations/'
+            )  
+        else:
+            # If the form is not valid, re-render the form with errors
+            return render(request, 'gas_station_form.html', {'form': form})
+    else:
+        # For GET request, render an empty form
+        form = GasStationForm()
+        return render(request, 'gas_station_form.html', {'form': form})
+
+
+def update_gas_station(request, gas_station_id):
+    gas_station = get_object_or_404(GasStation, id=gas_station_id)
+    if request.method == 'POST':
+        form = GasStationForm(request.POST, instance=gas_station)
+        if form.is_valid():
+            form.save()
+            # Redirect or render success page
+            return redirect('gas-stations/')
+    else:
+        return gas_station_form(request, 'update', gas_station)
+
+
+def delete_gas_station(request, gas_station_id):
+    gas_station = get_object_or_404(GasStation, id=gas_station_id)
+    if request.method == 'POST':
+        gas_station.delete()
+        # Redirect or render success page
+        return redirect('gas_stations')
+    else:
+        return gas_station_form(request, 'delete', gas_station)
+>>>>>>> cfe58171 (updated the gas station crud functionality)
