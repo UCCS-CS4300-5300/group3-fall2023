173a3f15e4d1a59bf0f3d2f0712c4d19a0573919
Gouthgithub <144191315+Gouthgithub@users.noreply.github.com>
2023-11-15 15:54:23 +0000

    created the owner , gas station, reviews ,and  inquiry  functionality thst spplies crud

--- station_tracker/forms.py
+++ station_tracker/forms.py
@@ -1,10 +1,13 @@
 from django import forms 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
 from . import models
 from django.forms import ModelForm, Textarea
 from .models import Feedback
 
+
+from .models import GasStationOwner, GasStationListing, GasStationReview, CustomerInquiry, GasStation
 class SignupForm(UserCreationForm):
   email = forms.EmailField(required=True,widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Email'}))
   
@@ -24,6 +27,7 @@ class SignupForm(UserCreationForm):
         return user
         
 class LoginForm(forms.Form):
+
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
@@ -44,3 +48,28 @@ class FeedbackForm(ModelForm):
       'gasStationAddr' : forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Gas Station Address', 'id':'gasStationAddr'}),
     }
  
+
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class GasStationOwnerForm(forms.ModelForm):
+    class Meta:
+        model = GasStationOwner
+        fields = ['owner_name', 'contact_number', 'email', 'business_address', 'emergency_contact']
+
+class GasStationListingForm(forms.ModelForm):
+    class Meta:
+        model = GasStationListing
+        fields = ['listing_id','station', 'station_owner']
+
+class GasStationReviewForm(forms.ModelForm):
+    class Meta:
+        model = GasStationReview
+        fields = ['gas_station', 'user_name', 'rating', 'review_text']
+
+class CustomerInquiryForm(forms.ModelForm):
+    class Meta:
+        model = CustomerInquiry
+        fields = ['gas_station', 'sender_name', 'sender_email', 'message_text', 'status']
+
