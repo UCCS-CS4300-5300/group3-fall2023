e6ed500c389548d7956642b291ec3c0fafc8fd29
gsanugom <26861430-gsanugom@users.noreply.replit.com>
2023-12-02 23:38:43 +0000

    j

--- gas_station_tracker/settings.py
+++ gas_station_tracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5nslf-su#bne0qz$&(8&eutx%7s=y9l5jc-h(4f=gh*s7e%1zt'
+SECRET_KEY = 'su#bne0qz$&(8&eutx%7s=y9l5jc-h(4f=gh*s7e%1zt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
